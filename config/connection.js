f925a2d3bb9e6d0dedcc8c64457d6da4860f8025
NIASKA <mkman751@yahoo.com>
2021-06-07 10:18:39 -0500

    test database

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'orjtv7oyp03uvqhr',
+    password: 'pihsv40ss7mqrbh8',
     dialect: 'mysql',
+    database: 'z8qq4t20bhgroauq',
     port: 3306
   });
 }
